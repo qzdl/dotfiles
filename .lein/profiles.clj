c9e1e04d641fa62d9f85824cb2e1559d54cd192f
qzdl <accounts@samuelculpepper.com>
2020-06-20 11:29:23 +0100

    lein, other remnants

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user
+ {:dependencies [[alembic "0.3.2"]
+                 [walmartlabs/system-viz "0.4.0"] ;; requires `graphviz`
+                 [datawalk "0.1.12"]
+                 [lein-oneoff "0.3.2"]]}}
