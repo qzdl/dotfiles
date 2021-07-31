6a24bd5890518a13fedcd8bdc7f72e7bd5448752
qzdl <accounts@samuelculpepper.com>
2021-07-31 18:48:38 +0100

    add all qzdl configs

---
+++ .config/PCSX2/bios/SCPH-77008_RUS_Con_0220_20060210_v15.bat
@@ -0,0 +1,3 @@
+@7za.exe a -t7z -mx7 -mse SCPH-77008_RUS_Con_0220_20060210_v15.P2b SCPH-77008_RUS_Con_0220_20060210_v15_[????????].*
+@if exist 7za.exe del /F SCPH-77008_RUS_Con_0220_20060210_v15_[????????].*
+@if exist 7za.exe del /F SCPH-77008_RUS_Con_0220_20060210_v15.bat
